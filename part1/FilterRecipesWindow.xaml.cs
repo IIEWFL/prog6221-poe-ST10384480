e2949af535f53a2e1c920a9d05c2e195731755e3
ST10384480 <131756565+ST10384480@users.noreply.github.com>
2024-06-27 10:38:00 +0200

    Add files via upload

---
+++ part1/FilterRecipesWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace RecipeApp
+{
+    public partial class FilterRecipesWindow : Window
+    {
+        public FilterRecipesWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
